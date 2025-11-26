dd5e6bbfd312d3dc55c7df7fbb96a69448adc432
Wyell Garay <23-01591@g.batstate-u.edu.ph>
2025-11-26 13:15:03 +0800

    Fixed URLs

--- src/user/urls.py
+++ src/user/urls.py
@@ -5,11 +5,12 @@ urlpatterns = [
     path("signup/", views.student_signup, name="signup"),
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
+    # Profile (student or organization)
+    path("<str:identifier>/", views.profile_view, name="profile_view"),
 
-    path('organization/<int:org_id>/follow/', views.follow_organization, name='follow_organization'),
-    path('organization/<int:org_id>/', views.profile_view, name='organization_profile'),
+    # Follow/unfollow organization
+    path("<str:identifier>/follow/", views.follow_organization, name="follow_organization"),
 
-    path('<str:identifier>/', views.profile_view, name='profile'),
-
+    # Organization tree
     path("organizations/tree/", views.org_tree, name="org_tree"),
 ]
